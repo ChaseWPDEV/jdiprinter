bce22eadec1e1157eacf59b2a0a92912aadf3b73
Chase Gru <chase@cwpdev.com>
2017-02-20 12:02:35 -0800

    Added delay logic

--- upload/jdidicom.py
+++ upload/jdidicom.py
@@ -7,6 +7,7 @@ import requests
 from importlib.machinery import SourceFileLoader
 import upload.globs as glob
 import os
+import time
 
 class JDIAE(AE):
 	url_base='http://localhost/'
@@ -17,15 +18,23 @@ class JDIAE(AE):
 		self.maximum_associations = 5
 		self.network_timeout=0
 		self.last_pt=None
+		self.last_time=0
 
 	def _cleanup_associations(self):
+		start=len(self.active_associations)
 		super(JDIAE, self)._cleanup_associations()
 		if len(self.active_associations)==0:
 			self.last_pt=None
+			if start>0:
+				self.last_time=time.time()
 
 	def on_c_store(self, dataset):
 		success=0x0000
 		if self.last_pt is not None:
+			return success
+		
+		delay=time.time()-self.last_time
+		if delay<5:
 			return success
 
 		ds=FileDataset('/tmp/1.dcm',dataset)
